b1f11a4595846e3b5fc69695e788c3383dc357b8
szalaytb <interpreter.szalay@gmail.com>
2026-05-19 18:05:29 +0200

    Updated clojure core

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.12.4"]
+  :dependencies [[org.clojure/clojure "1.12.5"]
                  [ring/ring-core "1.15.3"]
                  [ring/ring-jetty-adapter "1.15.3"]
                  [compojure "1.7.2"]
